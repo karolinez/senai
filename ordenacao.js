fdedb99272a64752f2524ba572e8e3da243bc23b
Karolinez <karolinesantos123.ks@gmail.com>
2021-04-17 14:10:32 -0300

    senai

---
+++ ordenacao.js
@@ -0,0 +1,27 @@
+
+// ordenação de vetores
+//for (i; i<5; i++){vet = Number(prompt("Informe um numero"))}
+
+    var vet = [6,4,8,2,7,1]
+    var i = 0
+    var j = 0
+    var aux = 0
+
+
+    for (i; i<6; i++) {
+    for (j=i+1; j<6; j++){
+        if (vet[i]>vet[j]){
+            aux = vet[i]
+            vet [i]= vet[j]
+            vet [j] = aux
+            
+        
+        }
+        
+    }
+    }
+console.log(vet)
+
+
+
+
